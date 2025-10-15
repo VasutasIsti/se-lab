874f0430076245c9af6d39d2e4c3a6627f8dab1a
vasutasisti <isti.nagy03@gmail.com>
2025-10-15 17:17:09 +0200

    Made changes according to PR

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -42,9 +42,6 @@ public class TorpedoStore {
       // successful firing
       this.torpedoCount -= numberOfTorpedos;
       success = true;
-    } else {
-      // simulated failure
-      success = false;
     }
 
     return success;
